1c0a4564986c2dc58c838e972445cfce6c4bde4f
DELL <punyasmrutin@gmail.com>
2021-05-03 13:04:37 +0530

    file modified

--- src/main/java/com/mightyjava/restcontroller/HelloController.java
+++ src/main/java/com/mightyjava/restcontroller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mightyjava.controller;
+package com.mightyjava.restcontroller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
